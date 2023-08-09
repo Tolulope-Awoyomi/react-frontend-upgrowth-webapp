41754c4de960af7ce991deb752ba79076d3f852e
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-08-09 14:34:14 -0400

    Create needed components

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>UPGROWTH WEB APP</h1>
+        <hr />
+        <h4> Grow in every aspects of your life</h4>
+    </div>
+  )
+}
+
+export default Home
