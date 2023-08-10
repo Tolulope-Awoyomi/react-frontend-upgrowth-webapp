e04e965b6ed5c0b956bda4146ae04cf02820e79c
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-08-10 13:55:38 -0400

    Build AspectLink Component to display each aspect as a Link

---
+++ src/components/AspectLink.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function AspectLink({aspect}) {
+  return (
+    <div>
+        <Link to={`/aspects/${aspect.id}`}>
+            <h3>{aspect.name}</h3>
+        </Link>
+    </div>
+  )
+}
+
+export default AspectLink
