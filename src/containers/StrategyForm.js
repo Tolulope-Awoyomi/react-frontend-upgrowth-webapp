50b0f93dd6fa9419da623e539c321d554933de4c
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-08-10 14:57:35 -0400

    Create a StrategyForm component

---
+++ src/containers/StrategyForm.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+function StrategyForm() {
+    const [name, setName] = useState("")
+  return (
+    <div>StrategyForm</div>
+  )
+}
+
+export default StrategyForm
